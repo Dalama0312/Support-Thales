53feda56767f9c035e90ac04db0b83fabf6f1c0c
Dalama0312 <52914591+Dalama0312@users.noreply.github.com>
2019-07-18 17:08:44 +0100

    new

---
+++ api/config.php
@@ -0,0 +1,6 @@
+<?php
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_PORT', '3306');
+define('MYSQL_USER', 'sa');
+define('MYSQL_PASSWORD', 'm@ster0312');
+define('MYSQL_DATABASE', 'dbsupport');
